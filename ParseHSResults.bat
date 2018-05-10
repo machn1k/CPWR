ef2061f975d1bc18dc8b54cc2ddbd9854069f09e
msingh9999 <39089792+msingh9999@users.noreply.github.com>
2018-05-10 08:13:47 -0400

    Update ParseHSResults.bat
    Corrected passing of arguments

--- ParseHSResults.bat
+++ ParseHSResults.bat
@@ -2,11 +2,11 @@
 
 echo Parsing RCPTT results for Host Services tests...
 
-c:\python27\python c:\python\RCPTTResultsFileParser.py C:\jenkins\workspace\RCPTT-Topaz-Nightly\com.compuware.mf.topaz.rcptt.hostservices-test\target\results\out.txt KB_HS "Topaz Workbench" 19.01.01 C:\jenkins\workspace\RCPTT-Topaz-Nightly\com.compuware.mf.topaz.rcptt.hostservices-test\target\results\results.properties
+c:\python27\python c:\python\RCPTTResultsFileParser.py C:\jenkins\workspace\RCPTT-Topaz-Nightly\com.compuware.mf.topaz.rcptt.hostservices-test\target\results\out.txt %1 "Topaz Workbench" 19.01.01 C:\jenkins\workspace\RCPTT-Topaz-Nightly\com.compuware.mf.topaz.rcptt.hostservices-test\target\results\results.properties
 
 echo Updating Zephyr tests...
 
-c:\python27\python c:\python\UpdateZephyrRCPTT.py C:\jenkins\workspace\RCPTT-Topaz-Nightly\com.compuware.mf.topaz.rcptt.hostservices-test\target\results\results.properties
+c:\python27\python c:\python\UpdateZephyrRCPTT.py C:\jenkins\workspace\RCPTT-Topaz-Nightly\com.compuware.mf.topaz.rcptt.hostservices-test\target\results\results.properties %2
 
 
 echo Done!
